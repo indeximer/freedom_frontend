1a0899d0596fc9ac94787a12888cfaf3c0526bb9
Douglas <douglasfortunato@hotmail.com>
2020-12-26 12:28:05 -0300

    init container orgnaization

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { useAuthenticationContext } from '@/contexts/Authentication'
+import { Link } from 'react-router-dom'
+import { useNavigation } from '@/hooks'
+import { useLoader } from '@/contexts/Loader'
+
+export function LoginContainer() {
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const { logIn, isAuthenticated } = useAuthenticationContext()
+  const { navigateTo } = useNavigation()
+  const { openLoader, closeLoader } = useLoader()
+
+  const handleLogin = async () => {
+    openLoader()
+    await logIn(email, senha)
+    closeLoader()
+  }
+
+  useEffect(() => {
+    if (isAuthenticated()) navigateTo('/')
+  }, [isAuthenticated, navigateTo])
+
+  return (
+    <form>
+      <h1>Login</h1>
+      <div>
+        <label>Email: </label>
+        <input
+          type="email"
+          name="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Senha: </label>
+        <input
+          type="password"
+          name="password"
+          onChange={e => setSenha(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="button" onClick={handleLogin}>
+          Logar
+        </button>
+      </div>
+      <p>
+        <Link to="/password-recovery">Recuperar senha</Link>
+      </p>
+      <p>
+        <Link to="/register">Ainda não tem uma conta? Registre-se!</Link>
+      </p>
+    </form>
+  )
+}
