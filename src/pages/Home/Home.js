480be6a647ad7a7460402480d12f867510cdef19
douglas <douglasfortunato@hotmail.com>
2021-03-20 12:46:56 -0300

    Remove skill creator btn

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Header } from '@/components/Header'
 import { Container } from '@/components/Container'
-import { SkillsCreator } from '@/containers/SkillsCreator'
 
 export function HomePage() {
   return (
     <Container>
       <Header pageTitle="Home" />
       <p>Home content soon...</p>
-      <SkillsCreator />
     </Container>
   )
 }
