46c1b17f995543e5613945596f44b629a2a94321
douglas <douglasfortunato@hotmail.com>
2021-05-02 12:01:02 -0300

    Add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const DIFFICULTY_LEVELS = {
+  0: 'Ridículo',
+  3: 'Fácil',
+  6: 'Razoável',
+  9: 'Complicado',
+  12: 'Difícil',
+  15: 'Muito Difícil',
+  18: 'Extremamente Difícil',
+  21: 'Praticamente Impossível',
+  24: 'Impossível',
+  27: 'Realmente Impossível',
+  30: 'Ridículo'
+}
