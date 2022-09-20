ece3a97a790550d86a521c9f7f3febd4f4dd6d7d
douglas <douglasfortunato@hotmail.com>
2022-09-20 15:46:09 -0300

    feat: configure caracter creation stepper

--- src/components/CharacterCreateProfile/CharacterCreateProfile.js
+++ src/components/CharacterCreateProfile/CharacterCreateProfile.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Typography, Avatar } from '@material-ui/core'
-import { StyledCharacterCreateProfile } from './styles'
+import { Typography, Grid } from '@material-ui/core'
 
 export function CharacterCreateProfile() {
   return (
-    <StyledCharacterCreateProfile>
-      <Avatar />
-      <Typography>Carregue a imagem do seu personagem</Typography>
-    </StyledCharacterCreateProfile>
+    <>
+      <Grid item>
+        <Typography>Carregue a imagem do seu personagem</Typography>
+      </Grid>
+    </>
   )
 }
--- src/containers/CharacterCreate/CharacterCreate.js
+++ src/containers/CharacterCreate/CharacterCreate.js
@@ -1,6 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Stepper,
+  Step,
+  StepLabel,
+  Button,
+  Hidden,
+  Grid
+} from '@material-ui/core'
+import CheckIcon from '@material-ui/icons/Check'
 import { CharacterCreateProfile } from '@/components/CharacterCreateProfile'
+import { Content } from '@/components/Content'
+import { Footer } from '@/components/Footer'
+
+const steps = ['Perfil', 'Habilidades', 'Poderes', 'Toques Finais']
 
 export function CharacterCreateContainer() {
-  return <CharacterCreateProfile />
+  const [activeStep, setActiveStep] = useState(0)
+
+  const handleNextStep = () =>
+    setActiveStep(prevActiveStep => prevActiveStep + 1)
+
+  const handleBackStep = () =>
+    setActiveStep(prevActiveStep => prevActiveStep - 1)
+
+  return (
+    <Content gutterBot={60}>
+      <Grid item xs={12}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => (
+            <Step key={index}>
+              <StepLabel>
+                <Hidden xsDown>{label}</Hidden>
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Grid>
+      <CharacterCreateProfile />
+      <Footer>
+        <Grid item xs={3}>
+          <Button
+            size="medium"
+            disabled={activeStep === 0}
+            onClick={handleBackStep}
+          >
+            Voltar
+          </Button>
+        </Grid>
+        <Grid item xs={5} lg={2}>
+          {activeStep !== steps.length - 1 && (
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              size="medium"
+              onClick={handleNextStep}
+            >
+              Avançar
+            </Button>
+          )}
+        </Grid>
+        <Grid item xs={4} lg={2}>
+          {activeStep === steps.length - 1 && (
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              size="medium"
+              startIcon={<CheckIcon />}
+              fullWidth
+            >
+              Finalizar
+            </Button>
+          )}
+        </Grid>
+      </Footer>
+    </Content>
+  )
 }
